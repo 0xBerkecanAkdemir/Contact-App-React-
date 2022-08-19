60ccd7ef8aab74195765c4374c5d0f3fa8c954c8
0xBerkecanAkdemir <berkeakdemirr@gmail.com>
2022-08-19 14:54:38 +0300

    filtered-completed

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -2,12 +2,20 @@ import {useState} from 'react'
 
 function List({contacts}) {
   const [filterText, setFilterText] = useState('');
+  const filtered = contacts.filter((item) => {
+    return Object.keys(item).some((key) => 
+      item[key]
+      .toString()
+      .toLowerCase()
+      .includes(filterText.toLocaleLowerCase)
+    );
+  });
   return (
     <div>
       <input placeholder='Filter contact'value={filterText} onChange={(e) => setFilterText(e.target.value)}/>
       <ul>
         {
-          contacts.map((contact, i) => <li key={i}>{contact.fullname}</li>)
+          filtered.map((contact, i) => <li key={i}>{contact.fullname}</li>)
         }
       </ul>
     </div>
